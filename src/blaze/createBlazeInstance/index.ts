f4cf1eecb4087cfaa6249a311dacb2cff51aa64a
SafraPC <leandrosafra.jconectada@gmail.com>
2022-07-02 00:59:34 -0300

    Create prettierrc and .env config

--- src/blaze/createBlazeInstance/index.ts
+++ src/blaze/createBlazeInstance/index.ts
@@ -1,12 +1,13 @@
-import { Connection } from "mysql2/typings/mysql";
-import blazeBot from "./app";
+import { Connection } from 'mysql2/typings/mysql'
+import blazeBot from './app'
+import 'dotenv/config'
 
 const init = async (connection: Connection) => {
-	try {
-		await blazeBot(connection);
-	} catch (e) {
-		console.log(e);
-	}
-};
+    try {
+        await blazeBot(connection)
+    } catch (e) {
+        console.log(e)
+    }
+}
 
-export default init;
+export default init
--- src/services/database.ts
+++ src/services/database.ts
@@ -1,21 +1,21 @@
-import mysql from "mysql2/promise";
-import Connection from "mysql2/typings/mysql/lib/Connection";
+import mysql from 'mysql2/promise'
+import Connection from 'mysql2/typings/mysql/lib/Connection'
 
 const connectMysql = async () => {
-  try {
-    const connection = await mysql.createConnection({
-      host: "127.0.0.1",
-      user: "root",
-      password: "root",
-      port: 3306,
-      database: "BlazeBOT",
-    });
-    console.log("Database connected!!!");
-    return { connection: connection };
-  } catch (err) {
-    console.log("Database connection error: " + err);
-    return { connection: null };
-  }
-};
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DB_SEED,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            port: Number(process.env.DB_PORT),
+            database: process.env.DB_HOST,
+        })
+        console.log('Database connected!!!')
+        return { connection: connection }
+    } catch (err) {
+        console.log('Database connection error: ' + err)
+        return { connection: null }
+    }
+}
 
-export { connectMysql };
+export { connectMysql }
